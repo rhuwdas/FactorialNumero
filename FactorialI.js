3de47d3a9cb0b1bb9f662a8d7eddd29f3781978e
rhuwdas <chimiemmanuel@gmail.com>
2026-09-07 10:47:45 -0600

    factorial Iterativo

---
+++ FactorialI.js
@@ -0,0 +1,16 @@
+
+function factorialFor(n) {
+  // El factorial de 0 o 1 es 1
+  if (n === 0 || n === 1) return 1;
+  
+  let resultado = 1;
+  
+  // Multiplica desde 2 hasta el número n
+  for (let i = 2; i <= n; i++) {
+    resultado *= i;
+  }
+  
+  return resultado;
+}
+
+console.log(factorialFor(5)); // Resultado: 120
