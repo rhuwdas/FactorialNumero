1db8a934c97a94fe007e60ebc4d93e2560f94711
rhuwdas <chimiemmanuel@gmail.com>
2026-09-08 11:35:35 -0600

    con mas salidas de consola

--- FactorialI.js
+++ FactorialI.js
@@ -15,3 +15,4 @@ function factorialFor(n) {
 
 console.log(factorialFor(5)); // Resultado: 120
 console.log(factorialFor(0)); // Resultado: 1
+console.assert(factorialFor(5) === 120, "El factorial de 5 debería ser 120");
