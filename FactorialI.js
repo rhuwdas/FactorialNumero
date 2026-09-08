dcd92c57c15c42c78feb89b526409f9313d2c6a4
rhuwdas <chimiemmanuel@gmail.com>
2026-09-08 11:47:16 -0600

    funcion serie factorial en consola

--- FactorialI.js
+++ FactorialI.js
@@ -16,3 +16,17 @@ function factorialFor(n) {
 console.log(factorialFor(5)); // Resultado: 120
 console.log(factorialFor(0)); // Resultado: 1
 console.assert(factorialFor(5) === 120, "El factorial de 5 debería ser 120");
+
+
+function mostrarSerieFactorial(limite) {
+    let factorial = 1;
+    
+    for (let i = 1; i <= limite; i++) {
+        factorial *= i;
+        console.log(`Factorial de ${i}: ${factorial}`);
+    }
+}
+
+// Ejemplo de uso para ver la serie hasta el número 6
+mostrarSerieFactorial(6);
+ 
